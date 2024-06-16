9bab5ffc064b85036a15e4d8a0d5f4e75000820f
ExtraPolite <imextrapolite2@gmail.com>
2024-06-16 18:04:52 -0400

    complete restaurant page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import home from './home';
 import menu from './menu';
+import about from './about';
 
 function showHide () {
   const home = document.querySelector('.home');
@@ -8,20 +9,25 @@ function showHide () {
   const menu = document.querySelector('.menu');
   const homePage = document.querySelector('.home-page');
   const menuPage = document.querySelector('.menu-page');
+  const aboutPage = document.querySelector('.about-page');
 
   home.addEventListener('click', () => {
     homePage.classList.remove('hide');
     menuPage.classList.add('hide');
     homePage.classList.add('show');
+    aboutPage.classList.add('hide');
   })
 
   menu.addEventListener('click', () => {
     menuPage.classList.remove('hide');
     homePage.classList.add('hide');
     menuPage.classList.add('show');
+    aboutPage.classList.add('hide');
   })
 
   about.addEventListener('click', () => {
+    aboutPage.classList.remove('hide');
+    aboutPage.classList.add('show');
     homePage.classList.add('hide');
     menuPage.classList.add('hide');
   })
@@ -29,4 +35,5 @@ function showHide () {
 
 home();
 menu();
+about();
 showHide();
