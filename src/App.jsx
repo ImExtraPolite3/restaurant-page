b10ea21ff5bf8221d37510168af63d907bd37976
ExtraPolite <imextrapolite2@gmail.com>
2024-09-15 00:56:09 -0400

    start to add functionality to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import Menu from './Components/Menu';
+import About from './Components/About';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Home />
+      <Menu />
+      <About />
     </>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,1 +1,31 @@
-export default function NavBar() {}
+function NavBarButtons({ id_name, button_name, hide_content }) {
+  return (
+    <button onClick={hide_content} id={id_name}>
+      {button_name}
+    </button>
+  );
+}
+
+function showHome() {
+  const allButtons = document.querySelectorAll('#nav-bar > button');
+
+  allButtons.forEach((button) => {
+    button.id === 'home'
+      ? (button.style.display = 'block')
+      : (button.style.display = 'none');
+  });
+}
+
+export default function NavBar() {
+  return (
+    <div id="nav-bar">
+      <NavBarButtons
+        hide_content={showHome}
+        id_name="home"
+        button_name="Home"
+      />
+      <NavBarButtons id_name="menu" button_name="Menu" />
+      <NavBarButtons id_name="about" button_name="About" />
+    </div>
+  );
+}
